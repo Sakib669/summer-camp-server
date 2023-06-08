4977c48fe14a4e119dea1cc29d3809cab2f36aeb
Sakib669 <stewieboyyy@gmail.com>
2023-06-08 11:31:03 -0700

    jwt added

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.use(express.json());
 
 
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access by verifyJWT'});
+  }
+
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access by verifyJWT'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 const uri = "mongodb+srv://summerCapmDB:IoZ1KJ0qPsc1QXUl@cluster0.wdjom0q.mongodb.net/?retryWrites=true&w=majority";
 
@@ -32,21 +48,27 @@ async function run() {
     const usersCollection = client.db('summerCapmDB').collection('users');
 
 
-    app.post('/users', async(req, res) => {
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const query = { email : user.email};
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
 
       const existingUser = await usersCollection.findOne(query);
 
-      if(existingUser){
-        return res.send({message: 'user already exists'});
+      if (existingUser) {
+        return res.send({ message: 'user already exists' });
       }
 
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
 
-    app.get('/users', async(req, res) => {
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray()
       res.send(result);
     })
